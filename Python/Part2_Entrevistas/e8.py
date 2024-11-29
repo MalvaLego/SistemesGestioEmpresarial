8ab0e9330b1f3f38d5af3087b24b174cc122f639
MalvaLego <152992436+MalvaLego@users.noreply.github.com>
2024-11-29 11:03:10 +0100

    Update e8.py

--- Python/Part2_Entrevistas/e8.py
+++ Python/Part2_Entrevistas/e8.py
@@ -3,19 +3,19 @@ import shutil
 
 llista=["png","mp4","doc","py","txt"]
 
-carpeta_actual = os.getcwd()
+carpetaActual = os.getcwd()
 
 for dir in llista:
     carpeta = dir
-    ruta = os.path.join(carpeta_actual, carpeta)
+    ruta = os.path.join(carpetaActual, carpeta)
     if not os.path.exists(ruta):
         os.mkdir(ruta)
 
-    for fitxer in os.listdir(carpeta_actual):
+    for fitxer in os.listdir(carpetaActual):
         if fitxer.endswith("."+dir):
-            ruta_fitxer = os.path.join(carpeta_actual, fitxer)
+            rutaFitxer = os.path.join(carpetaActual, fitxer)
             try:
-                shutil.move(ruta_fitxer, ruta)
+                shutil.move(rutaFitxer, ruta)
                 print("Fitxer "+fitxer+" mogut a "+ruta)
             except Exception as e:
                 print("Error al moure el fitxer "+fitxer+": "+e)
